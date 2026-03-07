0493adea0d1f656700a2bae677c924f32ae256b8
Yellareddy07 <131944447+Yellareddy07@users.noreply.github.com>
2026-03-07 11:01:22 +0530

    Update DockerSpringbootExApplicationTests.java
    REMOVED IMPORTS

--- src/test/java/com/example/DockerSpringbootEx/DockerSpringbootExApplicationTests.java
+++ src/test/java/com/example/DockerSpringbootEx/DockerSpringbootExApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.DockerSpringbootEx;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ALL)
 class DockerSpringbootExApplicationTests {
 
