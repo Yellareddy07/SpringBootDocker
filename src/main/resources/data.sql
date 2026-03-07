2fc4ef564230e40d40d5be75caa512d1fecdda18
Yellareddy07 <131944447+Yellareddy07@users.noreply.github.com>
2026-03-07 12:43:24 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,7 @@ INSERT INTO student(name,age,email) VALUES('Ravi',23,'raju@gamil.com');
 INSERT INTO student(name,age,email) VALUES('santhosh',24,'santhosh@gamil.com');
 INSERT INTO student(name,age,email) VALUES('Mani',20,'mani@gamil.com');
 INSERT INTO student(name,age,email) VALUES('sATYa',29,'satya@gamil.com');
+INSERT INTO student(name,age,email) VALUES('SIRI',24,'siri@gamil.com');
+
+
 
